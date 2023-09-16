be31b076662e8138cb2164077bd8b8527969eaa6
Mahonrri Mendez <mahonrri.mendez47@gmail.com>
2023-09-16 18:14:27 -0400

    first part of the program

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("Hello, please share a random number: ");
+        string x = Console.ReadLine();
+        int magicNumber = int.Parse(x);
+
+        Console.Write("What is your guess? ");
+        string y = Console.ReadLine();
+        int userGuess = int.Parse(y);
+
+
+        if (magicNumber == userGuess)
+        {
+            Console.WriteLine("You got it!! ");
+        }
+
+        else if (magicNumber > userGuess)
+        {
+            Console.WriteLine("Try higher ");
+        }
+
+        else if (magicNumber < userGuess)
+        {
+            Console.WriteLine("Try lower ");
+        }
     }
 }
