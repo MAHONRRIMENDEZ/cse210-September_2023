27d48667a981e5b3ac1631eee9b5043fd6e2f794
Mahonrri Mendez <mahonrri.mendez47@gmail.com>
2023-09-16 18:43:22 -0400

    Prep 3 completed

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,19 +5,21 @@ class Program
     static void Main(string[] args)
     {
         Random randomGenerator = new Random();
-        int magicNumber = randomGenerator.Next(1, 100);
+        int magicNumber = randomGenerator.Next(1, 101);
 
         Console.Write("Hello, lets play 'Guess the random number'");
         //string x = Console.ReadLine();
         //int magicNumber = int.Parse(x);
 
-        Console.Write("What is your guess? ");
-        string y = Console.ReadLine();
-        int userGuess = int.Parse(y);
 
+        int userGuess = -1;
 
         while (magicNumber != userGuess)
         {
+            Console.Write("What is your guess? ");
+            userGuess = int.Parse(Console.ReadLine());
+
+
             if (magicNumber == userGuess)
             {
             Console.WriteLine("You guess it!!");
