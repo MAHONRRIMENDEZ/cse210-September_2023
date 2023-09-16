bb60b7f7d3bce801a0baa6c0bc8753d822f09ea7
Mahonrri Mendez <mahonrri.mendez47@gmail.com>
2023-09-16 13:31:52 -0400

    added a coment

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
+        // this aslk for the user's name
         Console.Write("What is your first name? ");
         string first = Console.ReadLine();
 
