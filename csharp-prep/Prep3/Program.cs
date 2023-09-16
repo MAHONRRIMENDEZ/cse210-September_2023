89b4e21624bded7dca1b173cd4593750ffe39e08
Mahonrri Mendez <mahonrri.mendez47@gmail.com>
2023-09-16 18:37:59 -0400

    first attempt

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,28 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("Hello, please share a random number: ");
-        string x = Console.ReadLine();
-        int magicNumber = int.Parse(x);
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 100);
+
+        Console.Write("Hello, lets play 'Guess the random number'");
+        //string x = Console.ReadLine();
+        //int magicNumber = int.Parse(x);
 
         Console.Write("What is your guess? ");
         string y = Console.ReadLine();
         int userGuess = int.Parse(y);
 
 
-        if (magicNumber == userGuess)
+        while (magicNumber != userGuess)
         {
-            Console.WriteLine("You got it!! ");
+            if (magicNumber == userGuess)
+            {
+            Console.WriteLine("You guess it!!");
+            }
+
+            else if (magicNumber > userGuess)
+            {
+                Console.WriteLine("Try higher ");
+            }
+
+            else if (magicNumber < userGuess)
+            {
+                Console.WriteLine("Try lower ");
+            }
         }
 
-        else if (magicNumber > userGuess)
-        {
-            Console.WriteLine("Try higher ");
-        }
-
-        else if (magicNumber < userGuess)
-        {
-            Console.WriteLine("Try lower ");
-        }
     }
 }
