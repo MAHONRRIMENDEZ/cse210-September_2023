4c7e339758e58151040c34f7a8c177d3cbd70170
Mahonrri Mendez <mahonrri.mendez47@gmail.com>
2023-09-16 19:34:59 -0400

    1st attempt

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        int newNumber = -1;
+
+        List<int> numbers = new List<int>();
+
+        List<string> words = new List<string>();
+
+        while (newNumber != 0)
+            Console.WriteLine("What number would you like to append? ");
+            newNumber = int.Parse(Console.ReadLine());        
+
+
     }
-}
+} 
