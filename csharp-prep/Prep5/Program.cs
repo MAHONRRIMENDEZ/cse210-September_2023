aac81aedcac6e033ef9e0256e7d73facd2fac860
Mahonrri Mendez <mahonrri.mendez47@gmail.com>
2023-09-16 20:51:08 -0400

    1st try

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        Console.WriteLine("Welcome to the program!");
+
+        Console.WriteLine("Please Write your name: ");
+        string userName = Console.ReadLine();
+
+        Console.WriteLine("Please enter your favorite number: ");
+        int userNumber = 0;
+        userNumber = int.Parse(Console.ReadLine());
+
+        // calculating the square 
+
+        int square = userNumber * userNumber;
+
+        Console.WriteLine($"the square is {square}");
+
     }
 }
