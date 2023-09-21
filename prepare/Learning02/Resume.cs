7c29296a32cf185942d3d394c1361a0b13ff5a26
Mahonrri Mendez <mahonrri.mendez47@gmail.com>
2023-09-20 23:48:51 -0400

    second attempt

--- prepare/Learning02/Resume.cs
+++ prepare/Learning02/Resume.cs
@@ -1,5 +1,13 @@
 using System;
+using System.Threading.Tasks.Dataflow;
 public class Resume
 {
+    public string _name;
+    public List<Job> _jobs = new List<Job>();
+    job1._jobs.Add(job1);
     
+    public void DisplayResume()
+    {
+
+    }
 }
