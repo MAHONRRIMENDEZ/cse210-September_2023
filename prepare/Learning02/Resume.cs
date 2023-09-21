ed4534730f6437f5ad628dc0b337102ce9999874
Mahonrri Mendez <mahonrri.mendez47@gmail.com>
2023-09-20 23:59:46 -0400

    almost done

--- prepare/Learning02/Resume.cs
+++ prepare/Learning02/Resume.cs
@@ -3,11 +3,16 @@ using System.Threading.Tasks.Dataflow;
 public class Resume
 {
     public string _name;
+    public string _lastName;
     public List<Job> _jobs = new List<Job>();
-    job1._jobs.Add(job1);
-    
-    public void DisplayResume()
+    public void Display()
     {
+        Console.WriteLine($"Name: {_name} {_lastName}");
+        Console.WriteLine("Jobs: ");
 
+        foreach (Job job in _jobs)
+        {
+            job.Display();
+        }
     }
 }
