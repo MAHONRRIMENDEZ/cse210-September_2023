769a988d6fba7f1fc27e31cde4bc8eec0dbafee3
Mahonrri Mendez <mahonrri.mendez47@gmail.com>
2023-09-20 21:20:02 -0400

    2 jobs added as instances

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -5,12 +5,20 @@ class Program
     static void Main(string[] args)
     {
          Job job1 = new Job();
-         job1._company = "Dumbo";
-         job1._jobTitle = "waiter";
+         job1._company = "Dumbo Restaurant";
+         job1._jobTitle = "Waiter";
          job1._startYear = 2015;
          job1._endYear = 2018;
 
          job1.DisplayJob();
+
+         Job job2 = new Job();
+         job2._company = "Live Marketing Solutions";
+         job2._jobTitle = "Sales Man";
+         job2._startYear = 2018;
+         job2._endYear = 2019;
+
+         job2.DisplayJob();
 
     }
 }
