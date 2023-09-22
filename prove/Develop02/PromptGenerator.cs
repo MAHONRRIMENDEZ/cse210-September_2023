08c9ae0025cc52fc758bcd62b663688bef1e8499
Mahonrri Mendez <mahonrri.mendez47@gmail.com>
2023-09-21 23:01:11 -0400

    321

--- prove/Develop02/PromptGenerator.cs
+++ prove/Develop02/PromptGenerator.cs
@@ -1,12 +1,23 @@
-using System;
+using System.Collections.Generic;
 
 public class PromptGenerator
-{
-    public List<string> _prompts; //  = new List<Prompt>();
+{   
+    //public List<string> _prompts;
+    public List<string> _prompts = new List<string>();
+    
+    
 
     public string GetRandomPrompt()
     {
+        _prompts.Add("How am I feeling today?");
+        _prompts.Add("What are my top priorities for the day?");
+        _prompts.Add("What am I nervous or anxious about today?");
+        _prompts.Add("What did I learn today?");
         
+        Random rnd = new Random();
+        int index = rnd.Next(_prompts.Count);
+        Console.WriteLine(_prompts[index]);
+        return "";
         // Random randomGenerator = new Random();
         //string randomPrompt = randomGenerator.Next();
     }
