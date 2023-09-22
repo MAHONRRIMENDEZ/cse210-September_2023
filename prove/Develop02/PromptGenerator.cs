fd6eb5fb1b98ea7a4555ea5e9a7351279b0608f5
Mahonrri Mendez <mahonrri.mendez47@gmail.com>
2023-09-21 20:46:31 -0400

    setting the classes

---
+++ prove/Develop02/PromptGenerator.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class PromptGenerator
+{
+    public List<string> _prompts; //  = new List<Prompt>();
+
+    public string GetRandomPrompt()
+    {
+        
+        // Random randomGenerator = new Random();
+        //string randomPrompt = randomGenerator.Next();
+    }
+}
