c88af3e0f806338b6193c7cd4e1bae350b01b3ad
Mahonrri Mendez <mahonrri.mendez47@gmail.com>
2023-09-21 22:00:27 -0400

    123

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,11 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        int height;
+        Console.WriteLine("Welcome to the Journal Program! ");
+        Journal theJournal = new Journal();
 
-        height = 17;
-        height = 19;
+        int userComand = -1;
+        while (userComand != 5)
+        {
+            Console.WriteLine("Please select one of the following");
+            Console.WriteLine("1. Write");
+            Console.WriteLine("2. Display");
+            Console.WriteLine("3. Save");
+            Console.WriteLine("4. Load");
+            Console.WriteLine("5. Quit");
+            Console.Write("What would you like to do? ");
+            userComand = int.Parse(Console.ReadLine());
 
-        Console.Write(height);
+            if (userComand == 1)
+            {
+
+            }
+
+            if (userComand == 2)
+            {
+                
+            }
+
+            if (userComand == 3)
+            {
+                
+            }
+
+            if (userComand == 4)
+            {
+                
+            }      
+        }
+
     }
 }
