8c9b1f1332904b74272d0364e4409f781c1622dc
Mahonrri Mendez <mahonrri.mendez47@gmail.com>
2023-09-30 17:31:40 -0400

    123

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fractions f1 = new Fractions();
+        Console.WriteLine(f1.GetFractionString());
+        Console.WriteLine(f1.GetDecimalValue());
+
+        Fractions f2 = new Fractions();
+        Console.WriteLine(f2.GetFractionString());
+        Console.WriteLine(f2.GetDecimalValue());
     }
 }
