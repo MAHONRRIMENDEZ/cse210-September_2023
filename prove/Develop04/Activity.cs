f8568a84f1a3c3ae255dc7c0455085be38f2783d
Mahonrri Mendez <mahonrri.mendez47@gmail.com>
2023-10-07 18:37:25 -0400

    base code completed

---
+++ prove/Develop04/Activity.cs
@@ -0,0 +1,31 @@
+using System;
+
+public class Activity
+{
+    protected string _name = "";
+    protected string _description = "";
+    protected int _duration = 0;
+
+    public Activity()
+    {
+
+    }
+    public void DisplayStartingMessage()
+    {
+
+    }
+    public void DisplayEndingMessage()
+    {
+
+    }
+    public void ShowSpinner(int seconds)
+    {
+
+    }
+    public void ShowCountDown(int seconds)
+    {
+        
+    }
+
+
+}
