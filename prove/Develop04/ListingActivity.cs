c211e55ff585cdcbd5e00c20489e38f0219e4673
Mahonrri Mendez <mahonrri.mendez47@gmail.com>
2023-10-07 22:57:52 -0400

    listing

--- prove/Develop04/ListingActivity.cs
+++ prove/Develop04/ListingActivity.cs
@@ -10,18 +10,45 @@ public class ListingActivity : Activity
         _name = "Listing";
         _description = "This activity will help you reflect on the good things in your life by having you list as many things as you can in a certain area.";
         _duration = 0;
+   
+        _prompts.Add("Who are people that you appreciate?");
+        _prompts.Add("What are personal strengths of yours?");
+        _prompts.Add("Who are people that you have helped this week?");
+        _prompts.Add("When have you felt the Holy Ghost this month?");
+        _prompts.Add("Who are some of your personal heroes?");
     }
     public void Run()
     {
         DisplayStartingMessage();
     }
-    public void GetRandomPrompt()
+    public string GetRandomPrompt()
     {
-        
+        Random rnd = new Random();
+        int random = rnd.Next(0, _prompts.Count);
+        return _prompts[random]; 
+
     }
-    public void GetListFromUser()
+    public List<string> GetListFromUser()
     {
+
+        Console.WriteLine("List as many responses you can to the following prompt: ");     
+        Console.WriteLine($"---{GetRandomPrompt()}---");
+        Console.Write($"You may begin in: ");
+        ShowCountDown(5);
+        DateTime startTime = DateTime.Now;
+        DateTime endTime = startTime.AddSeconds(_duration); 
+
+
+        while (DateTime.Now < endTime)
+        {
+            Console.ReadLine();
+            ++_count;
+        }
         
+        Console.WriteLine($"You listed {_count} items!");
+        Console.WriteLine("Well Done!");
+        return _prompts;
+
     }
 
     
