5d9801c70af36da87f3ea83cfd235835609bb2f0
Mahonrri Mendez <mahonrri.mendez47@gmail.com>
2023-10-07 23:35:05 -0400

    Reflecting almost completed

--- prove/Develop04/ReflectingActivity.cs
+++ prove/Develop04/ReflectingActivity.cs
@@ -10,27 +10,69 @@ public class ReflectingActivity : Activity
     {
         _name = "Reflecting";
         _description = "This activity will help you reflect on times your life when you have shown strength and resilience. This will help you recognize the power you have and how you can use it in other aspects of your life. ";
-        _duration = 0;        
+        _duration = 0;  
+
+        //Prompts list
+        _prompts.Add("Think of a time when you stood up for someone else.");
+        _prompts.Add("Think of a time when you did something really difficult.");
+        _prompts.Add("Think of a time when you helped someone in need.");
+        _prompts.Add("Think of a time when you did something truly selfless.");      
+        _prompts.Add("Think of a time when you lerned something new about Jesus Christ");
+        
+         // Questions list
+        _questions.Add("Why was this experience meaningful to you?");
+        _questions.Add("Have you ever done anything like this before?");
+        _questions.Add("How did you get started?");
+        _questions.Add("How did you feel when it was complete?");
+        _questions.Add("What made this time different than other times when you were not as successful?");
+        _questions.Add("What is your favorite thing about this experience?");
+        _questions.Add("What could you learn from this experience that applies to other situations?");
+        _questions.Add("What did you learn about yourself through this experience?");
+        _questions.Add("How can you keep this experience in mind in the future?");
 
     }
     public void Run()
     {
         DisplayStartingMessage();
+        DisplayPrompt();
+        DisplayQuestions();
     }
     public string GetRandomPrompt()
     {
-        return "";
+        Random rnd = new Random();
+        int random = rnd.Next(0, _prompts.Count);
+        return _prompts[random]; 
+         
     }
     public string GetRandomQuestion()
     {
-        return "";
+        Random rnd = new Random();
+        int random = rnd.Next(0, _questions.Count);
+        return _questions[random]; 
     }
     public void DisplayPrompt()
     {
+        Console.WriteLine("Consider the following prompt:");
+        Console.WriteLine($"---{GetRandomPrompt()}---");
+        Console.WriteLine("When you have something in mind, press 'enter' to continue.");
+        Console.ReadLine(); //Enter request
 
+        Console.Write($"You may begin in: ");
+        ShowCountDown(5);
+        Console.WriteLine();
     }
     public void DisplayQuestions()
     {
+        DateTime startTime = DateTime.Now;
+        DateTime endTime = startTime.AddSeconds(_duration); 
+        while (DateTime.Now < endTime)
+        {
+            Console.Write($"> {GetRandomQuestion()}");
+            //Console.ReadLine();
+            ShowSpinner(10);
+        }
+
+
 
     }
     
