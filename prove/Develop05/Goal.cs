217422013bae5cbcb025d2e947fe25db44481bc7
Mahonrri Mendez <mahonrri.mendez47@gmail.com>
2023-10-14 10:59:30 -0400

    123

--- prove/Develop05/Goal.cs
+++ prove/Develop05/Goal.cs
@@ -1,5 +1,30 @@
 using System;
 public class Goal
 {
+    protected string _shortName;
+    protected string _description;
+    protected string _points;
+
+    public Goal(string name, string description, int points)
+    {
+
+    }
+    public void RecordEvent()
+    {
+
+    }
+    public bool IsComplete()
+    {
+        return true || false;
+    }
+    public string GetDetailsString()
+    {
+        return "";
+    }
+    public string GetStringRepresentation()
+    {
+        return "";
+    }
+
     
 }
