b33f3a70a4e588ab8dd76e180574819724d28029
Mahonrri Mendez <mahonrri.mendez47@gmail.com>
2023-10-14 10:58:24 -0400

    asd

--- prove/Develop05/SimpleGoal.cs
+++ prove/Develop05/SimpleGoal.cs
@@ -1,5 +1,23 @@
 using System;
 public class SimpleGoal : Goal
 {
-    
+    protected bool _isComplete;
+
+    public SimpleGoal(string name, string description, int points)
+    {
+        
+    }
+    public void RecordEvent()
+    {
+
+    }
+    public bool IsComplete()
+    {
+        return true || false;
+    }
+    public string GetStringRepresentation()
+    {
+        return "";
+    }
+
 }
